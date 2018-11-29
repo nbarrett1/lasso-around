f7e97f8b48701238bcc803b09f3b5bdc07796d00
ShanekaJay <shaneka@peak.net>
2018-11-29 17:52:36 +0000

    Adding cowboy movement

---
+++ lasso-around/Assets/CowboyMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowboyMovement : MonoBehaviour {
+
+	[SerializeField]
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(Vector3.zero, Vector3.forward, speed * Time.deltaTime);
+	}
+}
